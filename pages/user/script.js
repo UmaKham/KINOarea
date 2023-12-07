3914a45a218696942d852f4401887b8483e727ab
Ulmas Khamrokulov <uma.kham94@gmail.com>
2023-12-07 16:01:20 +0500

    fixed

--- pages/user/script.js
+++ pages/user/script.js
@@ -1,7 +1,17 @@
 let box = document.querySelector('.box')
 
 let settings_btn = document.querySelector('.settings')
+let settings_box = document.querySelector('.user_settings_box')
 
 settings_btn.onclick = () => {
   document.querySelector('.user_section').classList.add('hide')
+  settings_box.classList.remove('hide')
+  console.log(settings_box);
+}
+
+let save_btn = document.querySelector('.user_settings_box button')
+console.log(save_btn);
+save_btn.onclick = () => {
+  document.querySelector('.user_section').classList.remove('hide')
+  settings_box.classList.add('hide')
 }
