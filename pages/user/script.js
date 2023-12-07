d5cc756485f516a0594a413fab09441aaf73b544
Ulmas Khamrokulov <uma.kham94@gmail.com>
2023-12-07 13:46:45 +0500

    fixed

---
+++ pages/user/script.js
@@ -0,0 +1,7 @@
+let box = document.querySelector('.box')
+
+let settings_btn = document.querySelector('.settings')
+
+settings_btn.onclick = () => {
+  document.querySelector('.user_section').classList.add('hide')
+}
